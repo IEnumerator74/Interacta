5b26c9c16a7d68cf684ee6f9319bf74bd9159e76
IEnumerator74 <walteramato@gmail.com>
2024-11-09 21:04:26 +0100

    metodo hashrouter per risolvere probelma su mobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
 import Auth, { ProtectedRoute } from './components/Auth';
 import './index.css';
 
-// Determina il basename in base all'ambiente
-const basename = import.meta.env.DEV ? '/' : '/Interacta';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router basename={basename}>
+    <Router>
       <Routes>
         <Route path="/login" element={<Auth />} />
         <Route path="/" element={
